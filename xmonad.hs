3f7400c4603d41c4d853fdc2cd1664c7f838fb21
eiquai <eiquai@posteo.de>
2016-03-25 18:48:59 +0100

    use compton to end tearing

--- xmonad.hs
+++ xmonad.hs
@@ -20,16 +20,21 @@ import System.IO
 -- mod1mask -> [alt left]
 -- mod4mask -> [super]
 
+myStartupHook ::X ()
+myStartupHook = do
+    spawn "compton -f -I 0.10 -O 0.10 --backend glx --vsync opengl"
+
 main = do
     xmproc <- spawnPipe "xmobar"
 
 
     xmonad $ defaultConfig { 
         manageHook      = manageDocks <+> manageHook defaultConfig
+        , startupHook   = myStartupHook <+> startupHook defaultConfig
         , layoutHook    = avoidStruts $ layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        , ppTitle = xmobarColor "#F92672" "#272822" . shorten 100
                         }
         , modMask                 = myModMask
         , terminal                = myTerminal
@@ -40,13 +45,17 @@ main = do
         , normalBorderColor     = myNormalBorderColor
         , focusedBorderColor    = myFocusedBorderColor
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_l), spawn "xset dpms force off; physlock -ds")
+        [ ((mod4Mask .|. shiftMask, xK_l), spawn "sleep 1; xset dpms force off; physlock -ds")
         , ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume 1 +2db")
         , ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume 1 -2db")
         , ((0, xF86XK_AudioMute), spawn "pactl set-sink-mute 1 toggle")
+        , ((controlMask .|. mod1Mask, xK_t), spawn "urxvt")
+        , ((controlMask, xK_space), spawn "synapse")
+        , ((0, xF86XK_Tools), spawn "systemctl suspend")
+        , ((0, 0x1008FF21), spawn "systemctl suspend")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
-        , ((0, xK_Insert), pasteSelection)
+        , ((0, xK_Insert), pasteSelection) -- there is a problem here, as it seems to escape some characters
         ]
 --To Do:
 -- define keys shortcuts and hotkeys
