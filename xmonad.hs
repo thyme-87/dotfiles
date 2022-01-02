04d5333be843d1266b2a8ee5d08e544498b7b966
thyme-87 <thyme-87@posteo.me>
2022-01-02 11:41:44 +0100

    remove unused code

--- xmonad.hs
+++ xmonad.hs
@@ -104,8 +104,6 @@ myPP = xmobarPP
 --             , ppUrgent  =   xmobarColor myUrgentWSColor  "" . wrap myUrgentWSLeft myUrgentWSRight
 --             }
 
---myScreenLayoutHome = "xrandr --output VGA-0 --off --output LVDS-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-5 --mode 2560x1440 --pos 0x0 --rotate normal"
-
 myBar = "xmobar ~/dotfiles/xmobarrc"
 
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_m)
