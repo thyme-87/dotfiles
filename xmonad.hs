f606fdf62e2ed38154ff6d7ed19b115ae6be314b
thyme-87 <thyme-87@posteo.me>
2022-10-15 21:16:35 +0200

    fix background for i3lock

--- xmonad.hs
+++ xmonad.hs
@@ -124,7 +124,7 @@ myConfig = def {
         , normalBorderColor     = myNormalBorderColor
         , focusedBorderColor    = myFocusedBorderColor
         } `additionalKeys`
-        ([ ((mod4Mask .|. shiftMask, xK_l), spawn "i3lock -i ${HOME}/backgrounds/arch_background.png")
+        ([ ((mod4Mask .|. shiftMask, xK_l), spawn "i3lock --image=${HOME}/backgrounds/background-corrupted.png") --FIXME
         , ((mod1Mask        , xK_space), spawn "${HOME}/dotfiles/bin/layout_switch")
         , ((mod4Mask            , xK_m), sendMessage ToggleStruts)
         , ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ +4%")
